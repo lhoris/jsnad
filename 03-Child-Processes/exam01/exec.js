e0a0fcadfd0197213836d6f5bca629e6b09760e4
lhoris <lhoris@naver.com>
2024-08-11 13:59:26 +0900

    Child Processes

---
+++ 03-Child-Processes/exam01/exec.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+const { exec } = require('node:child_process');
+exec('cat *.js missing_file | wc -l', {encoding: 'UTF-8'}, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+});
